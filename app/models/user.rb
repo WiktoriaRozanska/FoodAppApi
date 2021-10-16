c8dfd10c5f3055b8c683c6dca57b7fff4a41753c
WiktoriaRozanska <wiktoria.rozanska1@gmail.com>
2021-10-16 19:23:00 +0200

    add devise and jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,22 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_secure_password
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: self
 
-  # VALIDATION
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
-  validates :password, confirmation: { case_sensitive: true }
-  validates :password, length: { minimum: 8 }
+  # # VALIDATION
+  # validates :password, presence: true
+  # validates :password_confirmation, presence: true
+  # validates :password, confirmation: { case_sensitive: true }
+  # validates :password, length: { minimum: 8 }
 
   # RELATIONSHIP
   has_many :recipes, inverse_of: :user
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,101 +10,122 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_210_926_104_542) do
+ActiveRecord::Schema.define(version: 2021_10_16_162813) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'days', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'position'
-    t.bigint 'week_day_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['week_day_id'], name: 'index_days_on_week_day_id'
+  create_table "days", force: :cascade do |t|
+    t.string "name"
+    t.integer "position"
+    t.bigint "week_day_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["week_day_id"], name: "index_days_on_week_day_id"
   end
 
-  create_table 'favorites', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'recipe_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['recipe_id'], name: 'index_favorites_on_recipe_id'
-    t.index ['user_id'], name: 'index_favorites_on_user_id'
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_favorites_on_recipe_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-  create_table 'ingredients', force: :cascade do |t|
-    t.string 'name'
-    t.bigint 'recipe_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['recipe_id'], name: 'index_ingredients_on_recipe_id'
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.bigint "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_ingredients_on_recipe_id"
   end
 
-  create_table 'recipe_tags', force: :cascade do |t|
-    t.bigint 'tag_id'
-    t.bigint 'recipe_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['recipe_id'], name: 'index_recipe_tags_on_recipe_id'
-    t.index ['tag_id'], name: 'index_recipe_tags_on_tag_id'
+  create_table "keywords", force: :cascade do |t|
+    t.bigint "recipe_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_keywords_on_recipe_id"
+    t.index ["tag_id"], name: "index_keywords_on_tag_id"
   end
 
-  create_table 'recipes', force: :cascade do |t|
-    t.string 'title'
-    t.integer 'cal_per_serv'
-    t.integer 'yields'
-    t.integer 'total_time'
-    t.string 'comment'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "recipe_tags", force: :cascade do |t|
+    t.bigint "tag_id"
+    t.bigint "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipe_id"], name: "index_recipe_tags_on_recipe_id"
+    t.index ["tag_id"], name: "index_recipe_tags_on_tag_id"
   end
 
-  create_table 'recipes_in_days', force: :cascade do |t|
-    t.bigint 'day_id'
-    t.bigint 'recipe_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['day_id'], name: 'index_recipes_in_days_on_day_id'
-    t.index ['recipe_id'], name: 'index_recipes_in_days_on_recipe_id'
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.integer "cal_per_serv"
+    t.integer "yields"
+    t.integer "total_time"
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'shop_lists', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'ingredient_id'
-    t.boolean 'bought', default: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['ingredient_id'], name: 'index_shop_lists_on_ingredient_id'
-    t.index ['user_id'], name: 'index_shop_lists_on_user_id'
+  create_table "recipes_in_days", force: :cascade do |t|
+    t.bigint "day_id"
+    t.bigint "recipe_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["day_id"], name: "index_recipes_in_days_on_day_id"
+    t.index ["recipe_id"], name: "index_recipes_in_days_on_recipe_id"
   end
 
-  create_table 'steps', force: :cascade do |t|
-    t.string 'description'
-    t.integer 'position'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "shop_lists", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "ingredient_id"
+    t.boolean "bought", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ingredient_id"], name: "index_shop_lists_on_ingredient_id"
+    t.index ["user_id"], name: "index_shop_lists_on_user_id"
   end
 
-  create_table 'tags', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "steps", force: :cascade do |t|
+    t.string "description"
+    t.integer "position"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'week_plans', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_week_plans_on_user_id'
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  create_table "week_plans", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_week_plans_on_user_id"
+  end
+
 end
